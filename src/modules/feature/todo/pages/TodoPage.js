27502ab9f2aae6fe912dd7c857d2936d803cb301
Victor Pinedo <victorpinedomartinez@gmail.com>
2021-12-24 11:32:59 -0500

    feat: add todoReducer

--- src/modules/feature/todo/pages/TodoPage.js
+++ src/modules/feature/todo/pages/TodoPage.js
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useReducer } from 'react';
+import { todoReducer } from '@core/reducers/todoReducer';
+
+const initialState = [{
+    id: new Date().getTime(),
+    description: "Study about RPA",
+    done: false
+}];
 
 export const TodoPage = () => {
+
+    const [todos] = useReducer(todoReducer, initialState);
+    console.log(todos);
+    
     return (
         <div>
             <h3>Todo List</h3>
             <hr />
+
+            <ul>
+                <li>Watch React udemy course</li>
+                <li>Go to workout</li>
+                <li>Spent time with family</li>
+                <li>Rest</li>
+            </ul>
         </div>
     )
 }

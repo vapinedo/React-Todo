c33515f7172df361dcdac0441811e6b14f567f98
Victor Pinedo <victorpinedomartinez@gmail.com>
2021-12-24 14:57:07 -0500

    Bootstrap 5 modal window added

--- src/modules/feature/todo/pages/TodoPage.js
+++ src/modules/feature/todo/pages/TodoPage.js
@@ -10,29 +10,51 @@ const initialState = [{
 export const TodoPage = () => {
 
     const [todos] = useReducer(todoReducer, initialState);
-    console.log(todos);
     
     return (
-        <div className="row todolist">
+        <>
+            <div className="row todolist">
+                <header className="todolist__header">
+                    <h3>Todo List ({todos.length})</h3>
 
-            <header className="todolist__header">
-                <h3>Todo List ({todos.length})</h3>
+                    <button className="btn btn-primary btn-sm" data-bs-toggle="modal" data-bs-target="#newTaskModal">
+                        New Task
+                    </button>
+                </header>
 
-                <button className="btn btn-primary btn-sm">New Task</button>
-            </header>
+                <ul>
+                    {todos.map(todo => (
+                        <li key={todo.id} className="todolist__item">
+                            <div>
+                                <i className='bx bx-circle todolist__checkitem'></i>
+                                {/* <i className='bx bx-check-circle todolist__checkitem'></i> */}
+                                <p>{todo.description}</p>
+                            </div>
+                            <i className='bx bx-trash-alt'></i>
+                        </li>
+                    ))}
+                </ul>
+            </div>
 
-            <ul>
-                {todos.map(todo => (
-                    <li key={todo.id} className="todolist__item">
-                        <div>
-                            <i class='bx bx-circle todolist__checkitem'></i>
-                            {/* <i class='bx bx-check-circle todolist__checkitem'></i> */}
-                            <p>{todo.description}</p>
+            <div className="modal fade" id="newTaskModal" aria-labelledby="newTaskModalLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="newTaskModalLabel">Modal title</h5>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
-                        <i class='bx bx-trash-alt'></i>
-                    </li>
-                ))}
-            </ul>
-        </div>
+
+                        <div className="modal-body">
+                        ...
+                        </div>
+
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-primary">Save</button>
+                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }

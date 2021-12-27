357c2734784f8e6e7f288ca86fc0d58e88aa93cc
Victor Pinedo <victorpinedomartinez@gmail.com>
2021-12-27 08:23:58 -0500

    todoReducer update to optimize

--- src/modules/core/reducers/todoReducer.js
+++ src/modules/core/reducers/todoReducer.js
@@ -9,16 +9,11 @@ export const todoReducer = (state = [], action) => {
             return state.filter(todo => todo.id !== action.payload);
 
         case "toggle":
-            return state.map(todo => {
-                if (todo.id === action.payload) {
-                    return {
-                        ...todo,
-                        done: !todo.done
-                    }
-                } else {
-                    return todo;
-                }
-            })
+            return state.map(todo => 
+                (todo.id === action.payload)
+                    ? { ...todo, done: !todo.done }
+                    : todo
+            );
 
         default: return state;
     }
